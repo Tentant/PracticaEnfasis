8aa44260559b71d354135e57cc30a24c1c2bd140
Santiago Arciniegas C <santiagoarciniegasocm@gmail.com>
2018-11-06 22:39:11 -0500

    Validación de sesión funcional
    Cuando no hay una sesión activa, redirige a la página de loggin. Falta revisar si está en todas las páginas.

---
+++ StoreUdem2.0/src/java/udem/edu/co/controller/PlantillaController.java
@@ -0,0 +1,32 @@
+package udem.edu.co.controller;
+
+import java.io.Serializable;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import udem.edu.co.entities.Users;
+
+@Named
+@ViewScoped
+public class PlantillaController implements Serializable{
+    
+    public void verificarSesion(){
+        try {
+            Object us =  FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+            //System.out.println(us.getIdrol().getIdrol());
+            if (us == null) {
+                System.out.println("no tiene nada");
+              FacesContext.getCurrentInstance().getExternalContext().redirect("../index.xhtml");
+            }else{
+                //FacesContext.getCurrentInstance().getExternalContext().redirect("viewTables.xhtml");
+            }
+            
+        } catch (Exception e) {
+           
+            System.out.println("Entre el catch");
+           
+        }
+        System.out.println("Voy a cambiar de página");
+        //return "permisos.xhtml";
+    }
+}
